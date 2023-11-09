87103304d3fc888863a37f49e7f4c6ec4ece5009
Fabian Nierop <165712@student.horizoncollege.nl>
2023-11-09 14:31:27 +0100

    update

--- account_creation.php
+++ account_creation.php
@@ -3,16 +3,16 @@
 session_start();
 
 $host = "localhost";
-    $dbname = "netland";
-    $username = "bit_academy";
-    $password = "bit_academy";
+$dbname = "netland";
+$username = "bit_academy";
+$password = "bit_academy";
 
-    try {
-        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
@@ -30,22 +30,23 @@ try {
 
     // Prepare the SQL statement
     $sql = "INSERT INTO users (username, password, email, type_of_user, first_name, last_name, company_name)
-            VALUES (:name, :password,  :email, :user_type, :first_name, :last_name, :company_name)"
+            VALUES (:name, :password,  :email, :user_type, :first_name, :last_name, :company_name)";
+
     $stmt = $pdo->prepare($sql);
-    
+
     $stmt->bindParam(':name', $name);
     $stmt->bindParam(':password', $hashedPassword);
-    
+
     // Hash the password
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-    
+
     // Execute the statement
     $stmt->execute();
-    
+
     // Redirect the user to a success page
     header("Location: index.php");
     exit();
-    
+
 } catch (\PDOException $e) {
     // Handle any database errors
     echo "Database Error: " . $e->getMessage();
