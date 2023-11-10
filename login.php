354107ec51dfed1664d5e177e25394669eeab832
Fabian Nierop <165712@student.horizoncollege.nl>
2023-11-10 18:15:16 +0100

    update

--- login.php
+++ login.php
@@ -83,8 +83,6 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
 </head>
 
 <body>
-    <?php include_once "navbar.php" ?>
-
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-6">
