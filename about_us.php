b96bf5ea4f75e4bcf0ea577fadf16affc6eae1fa
Fabian Nierop <165712@student.horizoncollege.nl>
2023-11-12 23:35:47 +0100

    removed some code which wasnt used anymore

---
+++ about_us.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us - JobSpot</title>
+    <!-- Tailwind CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.15/tailwind.min.css">
+</head>
+
+<body class="bg-gray-100">
+
+    <?php include_once "navbar.php" ?>
+
+    <main class="container mx-auto py-8 px-4">
+        <h2 class="text-center text-4xl font-bold mb-4">Welcome to JobSpot</h2>
+        <p class="text-center text-lg mb-4">Here at JobSpot, we are revolutionizing the job search experience. We understand the
+            importance of finding the perfect match between talented individuals and forward-thinking companies.</p>
+
+        <h3 class="text-center text-2xl font-bold mb-4">Our Services</h3>
+
+        <ul class="list-disc list-inside mb-4">
+            <li class="mb-2">Advanced algorithms for precise job seeker and job opportunity matching.</li>
+            <li class="mb-2">Dynamic company profiles showcasing values and work environment.</li>
+            <li class="mb-2">Streamlined tools for companies to post job applications and connect with candidates.</li>
+            <li class="mb-2">Detailed profiles for job seekers, including resumes and personal statements.</li>
+            <li class="mb-2">Fostering a sense of community, allowing professionals to connect and share insights.</li>
+        </ul>
+
+        <p class="text-center text-xl font-bold mb-4">"JobSpot: Igniting Careers, Connecting Futures"</p>
+    </main>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -22,9 +22,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jobspot - Find Your Next Job or Employee</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.7/tailwind.min.css">
-    <link rel="stylesheet" href="style.css">
 </head>
 
 <body class="bg-gray-100">
@@ -123,15 +121,6 @@ try {
         </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
-        </script>
 </body>
 
 </html>
