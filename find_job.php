6e932a48da62418f52c1bc1de06a0fa7d40c85bd
Fabian Nierop <165712@student.horizoncollege.nl>
2023-11-22 19:28:59 +0100

    moved around some code to make it less obfuscated

--- find_job.php
+++ find_job.php
@@ -1,7 +1,31 @@
 <?php
 
 include_once "connector.php";
-?>
+
+$sql = "SELECT id, company_name, field, job_type, location, hours_per_week, hourly_rate FROM job_postings";
+$result = $pdo->query($sql);
+
+// Loop through the job information and create a job card for each job
+$job_cards = "";
+if ($result->rowCount() > 0) {
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        $job_cards .= '<div class="bg-white rounded-lg shadow-lg p-4 mb-4 cursor-pointer" onclick="showDetails(' . $row["id"]
+            . ')" x-data="{ showDetails: false }">';
+        $job_cards .= '<h2 class="text-lg font-bold mb-2">' . $row["company_name"]
+            . '</h2>';
+        $job_cards .= '<p class="text-gray-700 mb-2">' . $row["field"] . '</p>';
+        $job_cards
+            .= '<p class="text-gray-700 mb-2">' . $row["job_type"] . '</p>';
+        $job_cards .= '<p class="text-gray-700 mb-2">' .
+            $row["location"] . '</p>';
+        $job_cards .= '<p class="text-gray-700 mb-2">' . $row["hours_per_week"]
+            . ' hours per week</p>';
+        $job_cards .= '<p class="text-gray-700 mb-2">$' . $row["hourly_rate"] . ' per hour</p>';
+        $job_cards
+            .= '<button class="bg-blue-500 text-white px-4 py-2 rounded-lg" @click="showDetails = true">View Details</button>';
+        $job_cards .= '</div>';
+    }
+} ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -29,68 +53,12 @@ include_once "connector.php";
     </script>
 </head>
 
+
+
 <body>
+
     <?php include_once "navbar.php" ?>
-    <div class="flex">
-        <!-- Left div with job cards -->
-        <div class="w-1/3 p-4">
-            <!-- Job card component -->
-            <div class="bg-white rounded-lg shadow-lg p-4 mb-4 cursor-pointer" x-data="{ showDetails: false }">
-                <h2 class="text-lg font-bold mb-2">Job Title</h2>
-                <p class="text-gray-700 mb-2">Job Description</p>
-                <p class="text-gray-700 mb-2">Job Location</p>
-                <button class="bg-blue-500 text-white px-4 py-2 rounded-lg" @click="showDetails = true">View
-                    Details</button>
-            </div>
-        </div>
-        <!-- Right div with detailed job information -->
-        <div class="w-2/3 p-4" x-show="showDetails">
-            <!-- Detailed job information component -->
-            <div class="bg-white rounded-lg shadow-lg p-4 mb-4">
-                <h2 class="text-lg font-bold mb-2">Job Title</h2>
-                <p class="text-gray-700 mb-2">Job Description</p>
-                <p class="text-gray-700 mb-2">Job Location</p>
-                <p class="text-gray-700 mb-2">Job Requirements</p>
-                <p class="text-gray-700 mb-2">Job Benefits</p>
-                <button class="bg-blue-500 text-white px-4 py-2 rounded-lg" @click="showDetails = false">Close
-                    Details</button>
-            </div>
-        </div>
-    </div>
-</body>
 
-</html>
-
-<?php
-$sql = "SELECT id, company_name, field, job_type, location, hours_per_week, hourly_rate FROM job_postings";
-$result = $pdo->query($sql);
-
-// Loop through the job information and create a job card for each job
-$job_cards = "";
-if ($result->rowCount() > 0) {
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        $job_cards .= '<div class="bg-white rounded-lg shadow-lg p-4 mb-4 cursor-pointer" onclick="showDetails(' . $row["id"]
-            . ')" x-data="{ showDetails: false }">';
-        $job_cards .= '<h2 class="text-lg font-bold mb-2">' . $row["company_name"]
-            . '</h2>';
-        $job_cards .= '<p class="text-gray-700 mb-2">' . $row["field"] . '</p>';
-        $job_cards
-            .= '<p class="text-gray-700 mb-2">' . $row["job_type"] . '</p>';
-        $job_cards .= '<p class="text-gray-700 mb-2">' .
-            $row["location"] . '</p>';
-        $job_cards .= '<p class="text-gray-700 mb-2">' . $row["hours_per_week"]
-            . ' hours per week</p>';
-        $job_cards .= '<p class="text-gray-700 mb-2">$' . $row["hourly_rate"] . ' per hour</p>';
-        $job_cards
-            .= '<button class="bg-blue-500 text-white px-4 py-2 rounded-lg" @click="showDetails = true">View Details</button>';
-        $job_cards .= '</div>';
-    }
-} ?>
-<!DOCTYPE html>
-<html lang="en">
-
-
-<body>
     <div class="flex">
         <!-- Left div with job cards -->
         <div class="w-1/3 p-4">
@@ -108,23 +76,36 @@ if ($result->rowCount() > 0) {
                     // Loop through the job information and create detailed job information for each job
                     if ($result->rowCount() > 0) {
                         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                ?>
-                                <div id="job-details-<?= $row["id"]; ?>" class="job-details" style="display: none;">
-                                <h2 class="text-lg font-bold mb-2"><?= $row["company_name"]; ?></h2>
-                                <p class="text-gray-700 mb-2"><?= $row["field"]; ?></p>
-                                <p class="text-gray-700 mb-2"><?= $row["job_type"]; ?></p>
-                                <p class="text-gray-700 mb-2"><?= $row["location"]; ?></p>
-                                <p class="text-gray-700 mb-2"><?= $row["hours_per_week"]; ?> hours per week</p>
-                                <p class="text-gray-700 mb-2">$<?= $row["hourly_rate"]; ?> per hour</p>
+                            ?>
+                            <div id="job-details-<?= $row["id"]; ?>" class="job-details" style="display: none;">
+                                <h2 class="text-lg font-bold mb-2">
+                                    <?= $row["company_name"]; ?>
+                                </h2>
+                                <p class="text-gray-700 mb-2">
+                                    <?= $row["field"]; ?>
+                                </p>
+                                <p class="text-gray-700 mb-2">
+                                    <?= $row["job_type"]; ?>
+                                </p>
+                                <p class="text-gray-700 mb-2">
+                                    <?= $row["location"]; ?>
+                                </p>
+                                <p class="text-gray-700 mb-2">
+                                    <?= $row["hours_per_week"]; ?> hours per week
+                                </p>
+                                <p class="text-gray-700 mb-2">$
+                                    <?= $row["hourly_rate"]; ?> per hour
+                                </p>
                             </div>
-                <?php
+                            <?php
                         }
                     }
                 } catch (PDOException $e) {
                     echo "Connection failed: " . $e->getMessage();
                 }
                 ?>
-                <button class="bg-blue-500 text-white px-4 py-2 rounded-lg" @click="showDetails = false">Close Details</button>
+                <button class="bg-blue-500 text-white px-4 py-2 rounded-lg" @click="showDetails = false">Close
+                    Details</button>
             </div>
         </div>
     </div>
