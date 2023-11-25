500d286388762c825e36f1bf11957c6f9ab0ffaa
Fabian Nierop <165712@student.horizoncollege.nl>
2023-11-26 00:36:59 +0100

    changed the php first to html first with the jobs cards

--- find_job.php
+++ find_job.php
@@ -5,26 +5,7 @@ include_once "connector.php";
 $sql = "SELECT id, company_name, field, job_type, location, hours_per_week, hourly_rate, description FROM job_postings";
 $result = $pdo->query($sql);
 
-// Loop through the job information and create a job card for each job
-$job_cards = "";
-if ($result->rowCount() > 0) {
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        $job_cards .= '<div class="bg-white rounded-lg shadow-lg p-4 mb-4 cursor-pointer" onclick="showDetails(' . $row["id"]
-            . ')" x-data="{ showDetails: false }">';
-        $job_cards .= '<h2 class="text-lg font-bold mb-2">' . $row["company_name"]
-            . '</h2>';
-        $job_cards .= '<p class="text-gray-700 mb-2">' . $row["field"] . '</p>';
-        $job_cards
-            .= '<p class="text-gray-700 mb-2">' . $row["job_type"] . '</p>';
-        $job_cards .= '<p class="text-gray-700 mb-2">' .
-            $row["location"] . '</p>';
-        $job_cards .= '<p class="text-gray-700 mb-2">' . $row["hours_per_week"]
-            . ' hours per week</p>';
-        $job_cards
-            .= '<button class="bg-blue-500 text-white px-4 py-2 rounded-lg" @click="showDetails = true">View Details</button>';
-        $job_cards .= '</div>';
-    }
-} ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -60,11 +41,36 @@ if ($result->rowCount() > 0) {
 
     <div class="flex">
         <!-- Left div with job cards -->
-        <div class="w-1/3 p-4" style="overflow:scroll; height:800px;">
-            <?= $job_cards; ?>
+        <div class="w-1/3 p-4" style="overflow:scroll; height:800px; margin-left:50px;">
+            <?php if ($result->rowCount() > 0): ?>
+                <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
+                    <div class="bg-white rounded-lg shadow-lg p-4 mb-4 cursor-pointer" onclick="showDetails(<?= $row['id']; ?>)"
+                        x-data="{ showDetails: false }">
+                        <h2 class="text-lg font-bold mb-2">
+                            <?= $row["company_name"]; ?>
+                        </h2>
+                        <p class="text-gray-700 mb-2">
+                            <?= $row["field"]; ?>
+                        </p>
+                        <p class="text-gray-700 mb-2">
+                            <?= $row["job_type"]; ?>
+                        </p>
+                        <p class="text-gray-700 mb-2">
+                            <?= $row["location"]; ?>
+                        </p>
+                        <p class="text-gray-700 mb-2">
+                            <?= $row["hours_per_week"]; ?> hours per week
+                        </p>
+                        <button class="bg-blue-500 text-white px-4 py-2 rounded-lg" @click="showDetails = true">View
+                            Details</button>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+
         </div>
         <!-- Right div with detailed job information -->
-        <div class="w-2/3 p-4" x-show="showDetails" style="display: flex; justify-content: center; align-items: center;">
+        <div class="w-2/3 p-4" x-show="showDetails"
+            style="display: flex; margin-right: 100px; margin-left: 50px; justify-content: center; align-items: center;">
             <!-- Detailed job information component -->
             <div class="bg-white rounded-lg shadow-lg p-4 mb-4">
                 <?php
