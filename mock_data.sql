f4dd3bd6b33ebd81cce8d7462d0308975d6673c2
Fabian Nierop <165712@student.horizoncollege.nl>
2023-12-06 15:21:03 +0100

    changed mockdata

--- mock_data.sql
+++ mock_data.sql
@@ -1,59 +1,11 @@
-USE jobspot;
-
-INSERT INTO job_postings (company_name, field, job_type, location, hours_per_week, hourly_rate, description)
+INSERT INTO employee_postings (qualifications, bio, looking_for, available_hours, user_id)
 VALUES
-  ('TechCorp', 'Information Technology', 'Software Engineer', 'San Francisco, CA', 40, 50.00, 'Join TechCorp as a Software Engineer and contribute to the development and maintenance of cutting-edge software applications. Collaborate with cross-functional teams to design, implement, and optimize software solutions that drive innovation.'),
+  ('Bachelor in Computer Science', 'Experienced software developer with expertise in Java and Python.', 'Seeking opportunities in software development.', 30, 1),
+  ('Master in Marketing', 'Creative and detail-oriented marketing professional with a passion for digital marketing strategies.', 'Open to marketing roles in dynamic companies.', 25, 1),
+  ('Ph.D. in Data Science', 'Data scientist with a strong background in machine learning and data analysis.', 'Looking for challenging projects in data science.', 20, 1);
 
-  ('HealthSolutions', 'Healthcare', 'Registered Nurse', 'New York, NY', 36, 45.00, 'Be a crucial part of the healthcare team at HealthSolutions as a Registered Nurse. Provide compassionate nursing care, administer medications, and collaborate with healthcare professionals to ensure the well-being of patients in a hospital setting.'),
-
-  ('GreenEnergy Co.', 'Renewable Energy', 'Environmental Engineer', 'Los Angeles, CA', 35, 55.00, 'Make a positive impact on the environment with GreenEnergy Co. as an Environmental Engineer. Design and implement sustainable solutions for renewable energy projects, focusing on minimizing environmental impact.'),
-
-  ('MarketingGuru', 'Marketing', 'Digital Marketing Specialist', 'Chicago, IL', 38, 60.00, 'Fuel the growth of MarketingGuru as a Digital Marketing Specialist. Plan and execute dynamic digital marketing campaigns, create engaging content, and analyze performance metrics to drive brand awareness and customer engagement.'),
-
-  ('FinancePros', 'Finance', 'Financial Analyst', 'Houston, TX', 40, 65.00, 'Join the finance team at FinancePros as a Financial Analyst. Conduct in-depth financial analysis, provide strategic insights, and contribute to informed decision-making to support the company''s financial objectives.'),
-
-  ('TechStart', 'Technology', 'Junior Developer', 'Seattle, WA', 32, 40.00, 'Kickstart your career in technology with TechStart as a Junior Developer. Assist in the development of software applications, work closely with experienced developers, and contribute to innovative projects.'),
-
-  ('HealthWell', 'Healthcare', 'Physical Therapist', 'Miami, FL', 35, 48.00, 'Empower patients'' health at HealthWell as a Physical Therapist. Provide therapeutic interventions, design personalized treatment plans, and collaborate with healthcare professionals to enhance patients'' physical well-being.'),
-
-  ('CleanEco', 'Environmental Services', 'Environmental Scientist', 'Denver, CO', 37, 52.00, 'Drive environmental sustainability with CleanEco as an Environmental Scientist. Conduct research, analyze data, and propose solutions to assess and minimize environmental impacts.'),
-
-  ('AdvertiseNow', 'Marketing', 'Social Media Manager', 'Atlanta, GA', 40, 55.00, 'Shape online presence with AdvertiseNow as a Social Media Manager. Manage social media accounts, create engaging content, and leverage analytics to optimize social media performance.'),
-
-  ('InvestPro', 'Finance', 'Investment Analyst', 'Dallas, TX', 38, 70.00, 'Navigate the world of investments with InvestPro as an Investment Analyst. Evaluate opportunities, analyze market trends, and prepare investment reports to guide strategic decision-making.'),
-
-  ('DataTech', 'Data Science', 'Data Analyst', 'Austin, TX', 35, 48.00, 'Transform data into insights at DataTech as a Data Analyst. Collect, analyze, and interpret data to provide actionable recommendations for business decision-making.'),
-
-  ('GreenSolutions', 'Renewable Energy', 'Solar Energy Engineer', 'Portland, OR', 36, 60.00, 'Harness the power of the sun with GreenSolutions as a Solar Energy Engineer. Design and implement solar energy systems for residential and commercial applications, contributing to a sustainable future.'),
-
-  ('MarketInsight', 'Marketing', 'Market Research Analyst', 'Philadelphia, PA', 40, 58.00, 'Uncover market trends with MarketInsight as a Market Research Analyst. Conduct studies, analyze data, and provide insights to inform strategic marketing decisions.'),
-
-  ('FinanceHub', 'Finance', 'Accountant', 'Phoenix, AZ', 37, 62.00, 'Maintain financial integrity with FinanceHub as an Accountant. Prepare financial statements, manage records, and ensure compliance with accounting standards.'),
-
-  ('InnoTech', 'Technology', 'UX/UI Designer', 'Raleigh, NC', 38, 55.00, 'Craft user-centric designs with InnoTech as a UX/UI Designer. Create visually appealing and functional solutions for web and mobile applications.'),
-
-  ('HealthCarePlus', 'Healthcare', 'Medical Laboratory Technologist', 'San Diego, CA', 35, 50.00, 'Contribute to healthcare diagnostics with HealthCarePlus as a Medical Laboratory Technologist. Perform tests, analyze results, and support medical professionals in accurate diagnoses.'),
-
-  ('EcoFriendlyHomes', 'Environmental Services', 'Energy Efficiency Consultant', 'Minneapolis, MN', 40, 65.00, 'Promote energy efficiency with EcoFriendlyHomes as an Energy Efficiency Consultant. Provide consultancy services to enhance energy efficiency in residential and commercial buildings.'),
-
-  ('DigitalAdPros', 'Marketing', 'Digital Advertising Specialist', 'Detroit, MI', 36, 58.00, 'Drive digital engagement with DigitalAdPros as a Digital Advertising Specialist. Plan and execute campaigns to enhance brand awareness and customer engagement through digital channels.'),
-
-  ('InvestWisely', 'Finance', 'Financial Planner', 'Nashville, TN', 40, 75.00, 'Guide financial futures with InvestWisely as a Financial Planner. Assist clients in financial planning, investment strategy, and wealth management.'),
-
-  ('TechInnovate', 'Technology', 'AI/Machine Learning Engineer', 'Salt Lake City, UT', 38, 70.00, 'Innovate with TechInnovate as an AI/Machine Learning Engineer. Develop and implement artificial intelligence and machine learning solutions for cutting-edge projects.'),
-
-  ('WellnessCare', 'Healthcare', 'Occupational Therapist', 'Cleveland, OH', 35, 52.00, 'Support well-being with WellnessCare as an Occupational Therapist. Provide therapy services to individuals with physical and cognitive challenges, promoting independence and quality of life.'),
-
-  ('GreenBuild', 'Renewable Energy', 'Wind Energy Technician', 'Kansas City, MO', 40, 55.00, 'Contribute to sustainable energy with GreenBuild as a Wind Energy Technician. Install, maintain, and repair wind turbines to ensure efficient and clean energy production.'),
-
-  ('TechInvent', 'Technology', 'Systems Architect', 'Las Vegas, NV', 40, 80.00, 'Shape the technological landscape with TechInvent as a Systems Architect. Design and implement scalable and efficient system architectures to meet business needs.'),
-
-  ('LifeCare', 'Healthcare', 'Mental Health Counselor', 'Orlando, FL', 35, 55.00, 'Support mental health with LifeCare as a Mental Health Counselor. Provide counseling services, conduct assessments, and develop treatment plans to assist individuals in achieving mental well-being.'),
-
-  ('GreenTechSolutions', 'Renewable Energy', 'Hydro Power Engineer', 'Seattle, WA', 37, 70.00, 'Contribute to hydroelectric power with GreenTechSolutions as a Hydro Power Engineer. Design, implement, and optimize hydro power systems to harness clean and renewable energy.'),
-
-  ('InfoInsight', 'Information Technology', 'Cybersecurity Analyst', 'Washington, DC', 38, 75.00, 'Secure digital landscapes with InfoInsight as a Cybersecurity Analyst. Analyze and respond to security threats, implement protective measures, and ensure the confidentiality of information.'),
-
-  ('MedTechInnovate', 'Healthcare', 'Biomedical Engineer', 'Boston, MA', 40, 68.00, 'Innovate in medical technology with MedTechInnovate as a Biomedical Engineer. Design and develop medical devices, ensuring their safety and effectiveness for healthcare applications.'),
-
-  ('EcoTransport', 'Environmental Services', 'Sustainability Analyst', 'San Francisco, CA', 35, 58.00, 'Promote eco-friendly transportation with EcoTransport as a Sustainability Analyst. Analyze and implement sustainable practices to reduce environmental impact in the transportation sector.');
+INSERT INTO job_postings (company_name, field, job_type, location, hours_per_week, hourly_rate, description, user_id)
+VALUES
+  ('ABC Company', 'Software Development', 'Full-time', 'City A', 40, 30.00, 'Looking for experienced software developers.', 1),
+  ('XYZ Corporation', 'Data Analysis', 'Part-time', 'City B', 20, 25.50, 'Data analyst position for a growing team.', 1),
+  ('123 Industries', 'Marketing', 'Contract', 'City C', 30, 18.75, 'Hiring a marketing specialist for a short-term project.', 1);
