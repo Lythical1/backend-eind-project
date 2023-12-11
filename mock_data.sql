59f5529b4f09fe04423986f02acc7eb142e29cea
Fabian Nierop <165712@student.horizoncollege.nl>
2023-12-11 10:57:33 +0100

    changed the seeder to directly use the mock_data sql file

--- mock_data.sql
+++ mock_data.sql
@@ -1,5 +1,8 @@
 INSERT INTO employee_postings (qualifications, bio, looking_for, available_hours, user_id)
 VALUES
+  ('Bachelors in Computer Science', 'I am a passionate developer...', 'Software Development opportunities', 20, 1),
+  ('Masters in Data Science', 'Experienced data scientist...', 'Data Science projects', 15, 1),
+  ('Web Development Bootcamp', 'Aspiring web developer...', 'Entry-level web development roles', 25, 1),
   ('Bachelor in Computer Science', 'Experienced software developer with expertise in Java and Python.', 'Seeking opportunities in software development.', 30, 1),
   ('Master in Marketing', 'Creative and detail-oriented marketing professional with a passion for digital marketing strategies.', 'Open to marketing roles in dynamic companies.', 25, 1),
   ('Ph.D. in Data Science', 'Data scientist with a strong background in machine learning and data analysis.', 'Looking for challenging projects in data science.', 20, 1);
@@ -8,4 +11,7 @@ INSERT INTO job_postings (company_name, field, job_type, location, hours_per_wee
 VALUES
   ('ABC Company', 'Software Development', 'Full-time', 'City A', 40, 30.00, 'Looking for experienced software developers.', 1),
   ('XYZ Corporation', 'Data Analysis', 'Part-time', 'City B', 20, 25.50, 'Data analyst position for a growing team.', 1),
-  ('123 Industries', 'Marketing', 'Contract', 'City C', 30, 18.75, 'Hiring a marketing specialist for a short-term project.', 1);
+  ('123 Industries', 'Marketing', 'Contract', 'City C', 30, 18.75, 'Hiring a marketing specialist for a short-term project.', 1),
+  ('Company A', 'Software Development', 'Full-time', 'City A', 40, 25.00, 'Lorem ipsum...', 1),
+  ('Company B', 'Data Science', 'Part-time', 'City B', 20, 30.00, 'Consectetur adipiscing elit...', 1),
+  ('Company C', 'Web Development', 'Contract', 'City C', 30, 22.00, 'Duis aute irure dolor...', 1);
--- seeder.php
+++ seeder.php
@@ -20,82 +20,8 @@ $insertAdminUser->bindParam(':type_of_user', $adminType);
 
 $insertAdminUser->execute();
 
-
-$jobPostings = [
-    [
-        'company_name' => 'Company A',
-        'field' => 'Software Development',
-        'job_type' => 'Full-time',
-        'location' => 'City A',
-        'hours_per_week' => 40,
-        'hourly_rate' => 25.00,
-        'description' => 'Lorem ipsum...',
-        'user_id' => 1,
-    ],
-    [
-        'company_name' => 'Company B',
-        'field' => 'Data Science',
-        'job_type' => 'Part-time',
-        'location' => 'City B',
-        'hours_per_week' => 20,
-        'hourly_rate' => 30.00,
-        'description' => 'Consectetur adipiscing elit...',
-        'user_id' => 1,
-    ],
-    [
-        'company_name' => 'Company C',
-        'field' => 'Web Development',
-        'job_type' => 'Contract',
-        'location' => 'City C',
-        'hours_per_week' => 30,
-        'hourly_rate' => 22.00,
-        'description' => 'Duis aute irure dolor...',
-        'user_id' => 1,
-    ],
-];
-
-$insertJobPosting = $pdo->prepare("
-    INSERT INTO job_postings (company_name, field, job_type, location, hours_per_week, hourly_rate, description, user_id)
-    VALUES (:company_name, :field, :job_type, :location, :hours_per_week, :hourly_rate, :description, :user_id)
-");
-
-foreach ($jobPostings as $jobPosting) {
-    $insertJobPosting->execute($jobPosting);
-}
-
-
-$employeePostings = [
-    [
-        'qualifications' => 'Bachelor\'s in Computer Science',
-        'bio' => 'I am a passionate developer...',
-        'looking_for' => 'Software Development opportunities',
-        'available_hours' => 20,
-        'user_id' => 1,
-    ],
-    [
-        'qualifications' => 'Master\'s in Data Science',
-        'bio' => 'Experienced data scientist...',
-        'looking_for' => 'Data Science projects',
-        'available_hours' => 15,
-        'user_id' => 1,
-    ],
-    [
-        'qualifications' => 'Web Development Bootcamp',
-        'bio' => 'Aspiring web developer...',
-        'looking_for' => 'Entry-level web development roles',
-        'available_hours' => 25,
-        'user_id' => 1,
-    ],
-];
-
-$insertEmployeePosting = $pdo->prepare("
-    INSERT INTO employee_postings (qualifications, bio, looking_for, available_hours, user_id)
-    VALUES (:qualifications, :bio, :looking_for, :available_hours, :user_id)
-");
-
-foreach ($employeePostings as $employeePosting) {
-    $insertEmployeePosting->execute($employeePosting);
-}
+$query = file_get_contents("mock_data.sql");
+$pdo->exec($query);
 
 echo "Seeder executed successfully.\n";
 
