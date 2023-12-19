0d5a78a50a6534ec7667ae585506e8d735b0e459
Fabian Nierop <165712@student.horizoncollege.nl>
2023-12-19 09:29:13 +0100

    update

--- my_applications.php
+++ my_applications.php
@@ -21,9 +21,8 @@ $user = $user_statement->fetch(PDO::FETCH_ASSOC);
 // Determine the user type (company or employee)
 $user_type = $user['type_of_user'];
 
-$sql = "SELECT id, company_name, field, contract_type, location, hours_per_week, hourly_rate, description FROM job_postings WHERE user_id = $user_id";
-$result = $pdo->query($sql);
 
+// Display the job postings
 ?>
 
 <!DOCTYPE html>
@@ -43,9 +42,9 @@ $result = $pdo->query($sql);
     <div class="flex">
         <!-- Left div with job cards -->
         <div class="w-1/3 p-4" style="overflow:scroll; height:92vh; margin-left:50px;">
-            <?php if ($user_type == 'employee') {
-                if ($result->rowCount() > 0): ?>
-                    <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
+            <?php if ($user_type == 'employee' || $user_type == 'admin') {
+                if ($result->rowCount() > 0):
+                    while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
                         <div class="bg-white rounded-lg shadow-lg p-4 mb-4 cursor-pointer" onclick="showDetails(<?= $row['id']; ?>)"
                             x-data="{ showDetails: false }">
                             <h2 class="text-lg font-bold mb-2">
@@ -62,12 +61,42 @@ $result = $pdo->query($sql);
                         </div>
                     <?php endwhile;
                 endif;
-            }
-            ?>
+            } ?>
+            <?php if ($user_type == 'company' || $user_type == 'admin') { ?>
+                <div class="w-1/3 p-4" style="overflow:scroll; height:92vh; margin-left:50px;">
+                    <?php if ($result->rowCount() > 0): ?>
+                        <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
+                            <div class="bg-white rounded-lg shadow-lg p-4 mb-4 cursor-pointer"
+                                onclick="showDetails(<?= $row['id']; ?>)" x-data="{ showDetails: false }">
+                                <h2 class="text-lg font-bold mb-2">
+                                    <?= $row["company_name"]; ?>
+                                </h2>
+                                <p class="text-gray-700 mb-2">
+                                    <?= $row["field"]; ?>
+                                </p>
+                                <p class="text-gray-700 mb-2">
+                                    <?= $row["contract_type"]; ?>
+                                </p>
+                                <p class="text-gray-700 mb-2">
+                                    <?= $row["location"]; ?>
+                                </p>
+                                <p class="text-gray-700 mb-2">
+                                    <?= $row["hours_per_week"]; ?> hours per week
+                                </p>
+                                <button class="bg-blue-500 text-white px-4 py-2 rounded-lg" @click="showDetails = true">View
+                                    Details</button>
+                            </div>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
 
+                </div>
+
+            <?php } ?>
         </div>
+        <!-- Right div with detailed job information -->
         <div class="w-2/3 p-4" x-show="showDetails"
             style="display: flex; margin-right: 100px; margin-left: 50px; justify-content: center; align-items: center;">
+            <!-- Detailed job information component -->
             <div class="bg-white rounded-lg shadow-lg p-4 mb-4">
                 <?php
                 try {
@@ -102,32 +131,35 @@ $result = $pdo->query($sql);
         </div>
     </div>
     <div>
-        <?php if ($user_type == 'company') {
-            if ($result->rowCount() > 0):
-                while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
-                    <div class="bg-white rounded-lg shadow-lg p-4 mb-4 cursor-pointer" onclick="showDetails(<?= $row['id']; ?>)"
-                        x-data="{ showDetails: false }">
-                        <h2 class="text-lg font-bold mb-2">
-                            <?= $row["company_name"]; ?>
-                        </h2>
-                        <p class="text-gray-700 mb-2">
-                            <?= $row["field"]; ?>
-                        </p>
-                        <p class="text-gray-700 mb-2">
-                            <?= $row["contract_type"]; ?>
-                        </p>
-                        <p class="text-gray-700 mb-2">
-                            <?= $row["location"]; ?>
-                        </p>
-                        <p class="text-gray-700 mb-2">
-                            <?= $row["hours_per_week"]; ?> hours per week
-                        </p>
-                        <button class="bg-blue-500 text-white px-4 py-2 rounded-lg" @click="showDetails = true">View
-                            Details</button>
-                    </div>
-                <?php endwhile;
-            endif;
-        } ?>
+        <?php foreach ($postings as $posting): ?>
+            <div id="job-details-<?= $posting["id"]; ?>" class="job-details" style="display: none;">
+                <h2 class="text-lg font-bold mb-2">
+                    <?= $posting["company_name"]; ?>
+                </h2>
+                <p class="text-gray-700 mb-2">
+                    <?= $posting["field"]; ?>
+                </p>
+                <p class="text-gray-700 mb-2">
+                    <?= $posting["contract_type"]; ?>
+                </p>
+                <p class="text-gray-700 mb-2">
+                    <?= $posting["location"]; ?>
+                </p>
+                <p class="text-gray-700 mb-2">
+                    <?= $posting["start_date"]; ?>
+                </p>
+                <p class="text-gray-700 mb-2">
+                    <?= $posting["hours_per_week"]; ?> hours per week
+                </p>
+                <p class="text-gray-700 mb-2">
+                    $
+                    <?= $posting["hourly_rate"]; ?> per hour
+                </p>
+                <p class="text-gray-700 mb-2">
+                    <?= $posting["description"]; ?>
+                </p>
+            </div>
+        <?php endforeach; ?>
     </div>
 
     <script>
@@ -143,11 +175,6 @@ $result = $pdo->query($sql);
             selectedJobDetail.style.display = 'block';
         }
     </script>
-</body>
-
-</html>
-<!-- END: ed8c6549bwf9 -->
-
 </body>
 
 </html>
